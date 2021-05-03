c8e8fe20759baf24dde57f5ffe7c6a293a5e1185
Satyabir Singh <satyabir.singh.osv@fedex.com>
2021-05-03 22:05:38 +0200

    api for Match data

--- src/main/java/personal/project/ipldashboard/controller/TeamController.java
+++ src/main/java/personal/project/ipldashboard/controller/TeamController.java
@@ -1,12 +1,13 @@
 package personal.project.ipldashboard.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import personal.project.ipldashboard.model.Match;
 import personal.project.ipldashboard.repository.MatchRepository;
 import personal.project.ipldashboard.repository.TeamRepository;
 import personal.project.ipldashboard.model.Team;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -26,5 +27,14 @@ public class TeamController {
         team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
 
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year)
+    {
+        LocalDate startDate = LocalDate.of(year,1,1);
+        LocalDate endDate = LocalDate.of(year+1,1,1);
+
+        return this.matchRepository.getMatchesByTeamBetweenDates(teamName,startDate,endDate);
     }
 }
